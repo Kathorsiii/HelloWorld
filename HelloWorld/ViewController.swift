9693f4ed25a451be95153ffdf4ef0e027af44834
Katherine Mariñas <55967594+Kathorsiii@users.noreply.github.com>
2021-07-10 22:29:39 +0800

    Update ViewController.swift

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -82,4 +82,15 @@ class ViewController: UIViewController {
         
         present(actionSheet, animated: true, completion: nil)
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        // To dismiss the keyboard on tap
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
 }
